c804440bbe8044de7cff8626350d8c4101df8b9f
Eduardo Mena <e.menatejeda@gmail.com>
2015-08-06 23:18:21 -0400

    Cheat sheet for javascript

---
+++ jshell.js
@@ -0,0 +1,71 @@
+// // 1.Variables
+
+// var num1 = 1010;
+// var num2 = 1722;
+
+// // 2.Debugging (alerts, comments, the console)
+
+// alert("Carpe Diem!");
+// console.log(num1 + num2);
+
+// // 3.Different data types
+
+// // String
+
+// var phrase1 ="Carpe Diem";
+
+// // Number
+
+// var num3 = 1089;
+
+// // Boolean
+
+// var x = true
+
+// // 4.Arrays
+
+// var months = ["October","August","May"]
+
+// // 5.Testing
+
+
+
+// // 6.Logic
+
+// var hour = 8;
+
+// if (hour < 12) {
+//     alert("Good Morning");
+// }
+
+
+// // 7.Functions
+
+// // function nameEnter(name) {
+// // 	return alert(name);
+// // }
+
+// nameEnter("Joel")
+
+
+// Part 2
+
+function heyDude(name) {
+  return alert(name);
+}
+
+heyDude("duuude");
+
+var num4 = 18;
+
+if (num4 <= 18) {
+	console.log("you should vote!");
+  } else {
+  	console.log("you cannot vote yet")
+}
+
+function subNumbers(num6, num7) {
+	return num6 - num7;
+}
+
+console.log(subNumbers(10, 5));
